aa2af180fe605c23afc84bc0b33073b2df1299dd
Boban Kilvin <btrajcevski10@gmail.com>
2023-07-26 00:49:48 -0400

    Add initial list and node factories

---
+++ linked-list.mjs
@@ -0,0 +1,10 @@
+const linkedListNode = (value = null, next = null) => {
+  return { value, next };
+};
+const LinkedList = () => {
+  let _head = null;
+  let length = 0;
+
+};
+
+export { LinkedList };
