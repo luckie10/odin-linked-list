deb5c97fae8934f39a29039114f92f59ec4c14dc
Boban Kilvin <btrajcevski10@gmail.com>
2023-07-26 00:53:09 -0400

    Add size and at functions

--- linked-list.mjs
+++ linked-list.mjs
@@ -1,6 +1,7 @@
 const linkedListNode = (value = null, next = null) => {
   return { value, next };
 };
+
 const LinkedList = () => {
   let _head = null;
   let length = 0;
@@ -30,11 +31,25 @@ const LinkedList = () => {
     }
   };
 
+  const size = () => length;
+
+  const at = (index) => {
+    if (index >= length) return null;
+
+    let pointer = _head;
+    for (let i = 0; i < index; i++) {
+      pointer = pointer.next;
+    }
+    return pointer;
+  };
+
   return {
     head,
     tail,
     prepend,
     append,
+    size,
+    at,
   };
 };
 
