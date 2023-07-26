ed83d04f87b42926d0e1d449b9fcfee080f6f16d
Boban Kilvin <btrajcevski10@gmail.com>
2023-07-26 00:52:25 -0400

    Add head and tail functions

--- linked-list.mjs
+++ linked-list.mjs
@@ -5,6 +5,21 @@ const LinkedList = () => {
   let _head = null;
   let length = 0;
 
+  const head = () => _head;
+
+  const tail = () => {
+    let pointer = _head;
+
+    while (pointer.next !== null) pointer = pointer.next;
+
+    return pointer;
+  };
+
+
+  return {
+    head,
+    tail,
+  };
 };
 
 export { LinkedList };
